4aefe16ecc4e0c91de3a9d7e7f9d8c8db61041c8
Abhay Gupta <iabhaygupta90@gmail.com>
2020-05-05 15:41:28 +0530

    Automatically add Rabbit MQ user

--- headlines-ms/src/docker-entrypoint.sh
+++ headlines-ms/src/docker-entrypoint.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
 
 rabbitmq-server start &
+rabbitmqctl add_user admin admin123
+rabbitmqctl set_user_tags admin administrator
+rabbitmqctl set_permissions -p / admin ".*" ".*" ".*"
 mongod --bind_ip_all
