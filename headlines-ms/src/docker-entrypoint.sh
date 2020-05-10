7d131f195eb9d7860680e992d714f455706ef3fb
Abhay Gupta <iabhaygupta90@gmail.com>
2020-05-10 10:37:57 +0530

    Automatically create admin user for RabbitMQ

--- headlines-ms/src/docker-entrypoint.sh
+++ headlines-ms/src/docker-entrypoint.sh
@@ -1,12 +1,10 @@
 #!/bin/sh
 
-rabbitmq-server start &
 mongod --bind_ip_all &
-touch /sample.txt &
-eval touch /sample2.txt &
-rabbitmqctl add_user admin admin123;
-rabbitmqctl set_user_tags admin administrator;
-rabbitmqctl set_permissions -p / admin  ".*" ".*" ".*";
-# rabbitmqctl add_user admin admin123 ; \
-# rabbitmqctl set_user_tags admin administrator ; \
-# rabbitmqctl set_permissions -p / admin  ".*" ".*" ".*" ; \
+rabbitmq-server start &
+# Sleeping, so that rabbitmq-server starts properly.
+(sleep 30;
+rabbitmqctl add_user admin admin123 2>/dev/null ; \
+rabbitmqctl set_user_tags admin administrator ; \
+rabbitmqctl set_permissions -p / admin  ".*" ".*" ".*" ; \
+echo "User 'admin' has been created") & rabbitmq-server
