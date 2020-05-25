b46c8310687f7802e9c0c0c4b026522c517374b8
Abhay Gupta <iabhaygupta90@gmail.com>
2020-05-25 13:28:28 +0530

    adding header, headlines and side nave components

--- front-end/bulletin/src/app/app.module.ts
+++ front-end/bulletin/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { SideNavComponent } from './side-nav/side-nav.component';
+import { HeadlinesComponent } from './headlines/headlines.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    SideNavComponent,
+    HeadlinesComponent
   ],
   imports: [
     BrowserModule,
