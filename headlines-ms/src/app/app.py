53730d8c1e8f203cffeed5db7a8a91a286113bb1
Abhay Gupta <iabhaygupta90@gmail.com>
2020-05-25 08:52:02 +0530

    Pulling headlines from Mongodb

--- headlines-ms/src/app/app.py
+++ headlines-ms/src/app/app.py
@@ -1,10 +1,23 @@
+import json
+
+from bson import json_util
 from flask import Flask
+from pymongo import MongoClient
+
+
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello():
-  return 'Hello world!'
+  mongo_client = MongoClient('mongodb://localhost:27017')
+  db = mongo_client.news
+  collection = db['headlines']
+  total_docs = collection.count_documents({})
+  print(total_docs, ' total documents.')
+  records = [document for document in collection.find({})]
+  return json.dumps(
+    records, sort_keys=True, indent=4, default=json_util.default)
 
 if __name__ == '__main__':
   app.run(host='0.0.0.0')
