c686294a5743915e8890e5c9cb1b1db20604cb8b
Abhay Gupta <iabhaygupta90@gmail.com>
2020-06-04 21:50:32 +0530

    Sleeping DD MS for 120 secs

--- data-distribution-ms/src/docker-entrypoint.sh
+++ data-distribution-ms/src/docker-entrypoint.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 rabbitmq-server start &
+sleep 120 &
 celery worker -A celery_dd_tasks.headlines.fetch_headlines --loglevel=info -f /app/celery-beat-logs/worker.log &
 celery -A celery_dd_tasks.headlines.fetch_headlines beat --loglevel=info -f /app/celery-beat-logs/beat.log &
 celery worker -A celery_dd_tasks.covid19.fetch_covid19_news --loglevel=info -f /app/celery-beat-logs/worker.log &
