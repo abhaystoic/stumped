6fa0cbba6daa68b94b2d17e2912c3e01e3d204cf
Abhay Gupta <iabhaygupta90@gmail.com>
2020-06-07 10:32:24 +0530

    adding all API services

--- front-end/bulletin/src/app/api.service.ts
+++ front-end/bulletin/src/app/api.service.ts
@@ -8,13 +8,33 @@ export class ApiService {
 
   constructor(private httpClient: HttpClient) { }
 
-  public getHeadlines() {
-  	return this.httpClient.get('http://172.22.0.4:5000');
+  public getBusinessNews() {
+  	return this.httpClient.get('http://localhost:5000/business');
   }
 
   public getCovid19News() {
   	console.log('getCovid19News');
   	return this.httpClient.get('http://172.22.0.3:5000');
+  }
+
+  public getEntertainmentNews() {
+  	return this.httpClient.get('http://localhost:5000/entertainment');
+  }
+
+  public getHeadlines() {
+  	return this.httpClient.get('http://172.22.0.4:5000');
+  }
+
+  public getHealthNews() {
+  	return this.httpClient.get('http://localhost:5000/health');
+  }
+
+  public getScienceNews() {
+  	return this.httpClient.get('http://localhost:5000/science');
+  }
+
+  public getSportsNews() {
+  	return this.httpClient.get('http://localhost:5000/sports');
   }
 
   public getTechnologyNews() {
