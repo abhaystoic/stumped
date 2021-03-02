a1c378ad051d85b18d90b6944ea75ac0ba41f473
Abhay Gupta <iabhaygupta90@gmail.com>
2021-03-02 21:11:35 +0530

    :imp: Social login plugin integration.

--- front-end/bulletin/src/app/app.module.ts
+++ front-end/bulletin/src/app/app.module.ts
@@ -9,18 +9,29 @@ import { HeaderComponent } from './header/header.component';
 import { SideNavComponent } from './side-nav/side-nav.component';
 import { HeadlinesComponent } from './headlines/headlines.component';
 import { Covid19Component } from './covid19/covid19.component';
-import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import {
+  PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { TechnologyComponent } from './technology/technology.component';
 import { SportsComponent } from './sports/sports.component';
-import { EntertainmentComponent } from './entertainment/entertainment.component';
+import {
+  EntertainmentComponent } from './entertainment/entertainment.component';
 import { ScienceComponent } from './science/science.component';
 import { BusinessComponent } from './business/business.component';
 import { HealthComponent } from './health/health.component';
-import { ArticleCardComponent } from './shared/article-card/article-card.component';
+import { 
+  ArticleCardComponent } from './shared/article-card/article-card.component';
 import { ContactUsComponent } from './contact-us/contact-us.component';
 import { AboutUsComponent } from './about-us/about-us.component';
 import { SearchComponent } from './search/search.component';
 import { MyProfileComponent } from './my-profile/my-profile.component';
+
+/**
+ * Social Login.
+ */
+import {
+  SocialLoginModule, SocialAuthServiceConfig } from 'angularx-social-login';
+import {
+  GoogleLoginProvider, FacebookLoginProvider } from 'angularx-social-login';
 
 @NgModule({
   declarations: [
@@ -46,9 +57,27 @@ import { MyProfileComponent } from './my-profile/my-profile.component';
     AppRoutingModule,
     BrowserModule,
     FormsModule,
-    HttpClientModule
+    HttpClientModule,
+    SocialLoginModule
   ],
-  providers: [],
+  providers: [
+    {
+      provide: 'SocialAuthServiceConfig',
+      useValue: {
+        autoLogin: true,
+        providers: [
+          {
+            id: GoogleLoginProvider.PROVIDER_ID,
+            provider: new GoogleLoginProvider('230666856260-7vfam2ohmhq1rc1buf2g50v2jlrj6ivu.apps.googleusercontent.com')
+          },
+          {
+            id: FacebookLoginProvider.PROVIDER_ID,
+            provider: new FacebookLoginProvider('3817232428330320')
+          }
+        ]
+      } as SocialAuthServiceConfig,
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- front-end/bulletin/src/app/my-profile/my-profile.component.ts
+++ front-end/bulletin/src/app/my-profile/my-profile.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 
+import { SocialAuthService, SocialUser } from "angularx-social-login";
+import {
+  FacebookLoginProvider, GoogleLoginProvider } from "angularx-social-login";
+
 @Component({
   selector: 'app-my-profile',
   templateUrl: './my-profile.component.html',
@@ -7,9 +11,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MyProfileComponent implements OnInit {
 
-  constructor() { }
+  user: SocialUser;
+  loggedIn: boolean;
+
+  constructor(private authService: SocialAuthService) { }
 
   ngOnInit(): void {
+    this.authService.authState.subscribe((user) => {
+      this.user = user;
+      this.loggedIn = (user != null);
+      console.log(user);
+    });
+  }
+
+  signInWithGoogle(): void {
+    this.authService.signIn(GoogleLoginProvider.PROVIDER_ID);
+  }
+
+  signInWithFB(): void {
+    this.authService.signIn(FacebookLoginProvider.PROVIDER_ID);
+  }
+
+  signOut(): void {
+    this.authService.signOut();
   }
 
 }
