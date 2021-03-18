1df06d12e314c86890ea700708196eedcb65c0ea
Abhay Gupta <iabhaygupta90@gmail.com>
2021-03-18 16:41:00 +0530

    :running: News Article page for every article.

---
+++ front-end/bulletin/src/app/news-article/news-article.component.ts
@@ -0,0 +1,85 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+import { SocialAuthService, SocialUser } from "angularx-social-login";
+
+import { ApiService } from '../api.service';
+import { CommonFunctionsService } from '../common-functions.service';
+import { SplashService } from '../splash.service';
+
+@Component({
+  selector: 'app-news-article',
+  templateUrl: './news-article.component.html',
+  styleUrls: ['./news-article.component.css', '../global.css']
+})
+export class NewsArticleComponent implements OnInit {
+
+  newsArticle: [];
+  slug: string;
+  source: string;
+  loggedIn: boolean;
+  pageName: string = 'news-article';
+  showSplash: boolean = true;
+  user: SocialUser;
+  constructor(
+    private apiService: ApiService,
+    private authService: SocialAuthService,
+    private commonFunctionsService: CommonFunctionsService,
+    private route:ActivatedRoute,
+    private splashService: SplashService) {
+      this.slug = this.route.snapshot.params['slug'];
+      this.source = this.route.snapshot.params['source'];
+      this.authService.authState.subscribe(user => {
+        this.user = user;
+        this.loggedIn = (user != null);
+        this.fetchNewsArticle();
+      });
+    }
+
+  ngOnInit(): void {}
+
+  fetchNewsArticle(): void {
+    this.splashService.updateSplashState(true);
+    this.apiService.getNewsArticle(this.slug, this.source).subscribe( async (data) => {
+      data = new Array(data)
+      if (this.user) {
+        // Add articles that were saved by the user earlier.
+        this.newsArticle = 
+          await this.commonFunctionsService.addSavedArticlesDetailsToNews(
+            this.user.email, this.user.provider, this.pageName, data);
+      } else {
+        data[0]['savedArticle'] = false;
+        this.newsArticle = data;
+      }
+      this.splashService.updateSplashState(false);
+    });
+  }
+
+  async saveNewsArticle(newsId: string) {
+    let response = await this.apiService.saveNews(
+      this.user.email, this.user.provider, newsId).toPromise();
+    if (response['success']) {
+      return true;
+    }
+    return false;
+  }
+
+  async unSaveNewsArticle(newsId: string) {
+    let response = await this.apiService.unSaveNews(
+      this.user.email, this.user.provider, newsId).toPromise();
+    if (response['success']) {
+      return true;
+    }
+    return false;
+  }
+
+  async saveNewsArticleSentiment(newsId: string, sentiment: string) {
+    let response = await this.apiService.saveSentiment(
+      this.user.email, this.user.provider, newsId, sentiment).toPromise();
+    if (response['success']) {
+      return true;
+    }
+    return false;
+  }
+
+}
